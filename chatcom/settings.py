00449d7450aeda646bd278ecc08bb85b0906619f
ecknyce <phiriecknycem@gmail.com>
2026-05-05 09:43:56 +0200

    Fixed allowed origin issues

--- chatcom/settings.py
+++ chatcom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOST_NAME"), "localhost","127.0.0.1" ]
+ALLOWED_HOSTS = [f"{os.environ.get('RENDER_EXTERNAL_HOST_NAME')}", "localhost", "127.0.0.1", '0.0.0.0']
 
 
 # Application definition
