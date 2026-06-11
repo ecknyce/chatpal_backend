a725c5d2af0746744fbe34add23507ae3c87492e
ecknyce <phiriecknycem@gmail.com>
2026-06-11 09:58:01 +0200

    Switched to supabase database

--- chatcom/settings.py
+++ chatcom/settings.py
@@ -148,18 +148,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3',
+#         conn_max_age=600
+#     )
+# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': config('SUPABASE_PASSWORD'),
+        'HOST': config('SUPABASE_HOST'),
+        'PORT': '5432',
+    }
 }
 
 
